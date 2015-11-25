bf471f9455fae12fe0401485258d6545c9795e17
taihangy <yetaihang.zju@gmail.com>
2015-11-25 10:47:26 -0500

    function almost done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  skip_before_filter :authorize, only: [:new, :create, :update, :destroy]
+  skip_before_filter :authorize
   before_action :set_cart, only: [:edit, :update, :destroy]
 
   # GET /carts
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -36,7 +36,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         # session[:counter] = 0
-        format.html { redirect_to store_url }
+        format.html { redirect_to @line_item.cart }
         format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
